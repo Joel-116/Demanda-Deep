d8237e2facf8b8f25acc55515ef973af3f0ffd91
João Emanoel <137958162+Joel-116@users.noreply.github.com>
2024-07-10 22:35:22 -0300

    Delete petrobras directory

--- petrobras/rastreamento.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('trackingForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var trackingNumber = document.getElementById('trackingNumber').value.trim();
-    if (trackingNumber !== '') {
-        document.getElementById('trackingResults').innerHTML = '<p>O produto com código ' + trackingNumber + " está em trânsito.</p>";
-    } else {
-        alert("Por favor, digite um código de rastreamento válido.");
-    }
-});
